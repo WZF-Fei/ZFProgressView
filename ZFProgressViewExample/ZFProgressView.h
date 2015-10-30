70cd1eff340cb957602d274105e0d3f98d1d0374
Damon <Damon@imacdeiMac.local>
2015-10-30 09:13:46 +0800

    使用IB_DESIGNABLE和IBInspectable在IB中显示控件

--- ZFProgressViewExample/ZFProgressView.h
+++ ZFProgressViewExample/ZFProgressView.h
@@ -15,25 +15,26 @@ typedef NS_ENUM(NSInteger, ZFProgressViewStyle) {
 
 };
 
+IB_DESIGNABLE
 @interface ZFProgressView : UIView
 
 //进度线条宽度
-@property (nonatomic,assign) CGFloat progressLineWidth;
+@property (nonatomic,assign) IBInspectable CGFloat progressLineWidth;
 //背景线条宽度
-@property (nonatomic,assign) CGFloat backgourndLineWidth;
+@property (nonatomic,assign) IBInspectable CGFloat backgourndLineWidth;
 //进度百分比
-@property (nonatomic,assign) CGFloat Percentage;
+@property (nonatomic,assign) IBInspectable CGFloat Percentage;
 //背景填充颜色
-@property (nonatomic,strong) UIColor *backgroundStrokeColor;
+@property (nonatomic,strong) IBInspectable UIColor *backgroundStrokeColor;
 //进度条填充颜色
-@property (nonatomic,strong) UIColor *progressStrokeColor;
+@property (nonatomic,strong) IBInspectable UIColor *progressStrokeColor;
 //距离边框边距偏移量
-@property (nonatomic,assign) CGFloat offset;
+@property (nonatomic,assign) IBInspectable CGFloat offset;
 //步长
-@property (nonatomic,assign) CGFloat step;
+@property (nonatomic,assign) IBInspectable CGFloat step;
 
 //数字字体颜色
-@property (nonatomic,strong) UIColor *digitTintColor;
+@property (nonatomic,strong) IBInspectable UIColor *digitTintColor;
 
 /*
  设置每个格子的宽度与线条宽度要注意
@@ -45,9 +46,9 @@ typedef NS_ENUM(NSInteger, ZFProgressViewStyle) {
 
 
 //格子之间的宽度
-@property (nonatomic,assign) CGFloat GapWidth;
+@property (nonatomic,assign) IBInspectable CGFloat GapWidth;
 //设置线条宽度 （进度条宽度 = 背景线条宽度）
-@property (nonatomic,assign) CGFloat lineWidth;
+@property (nonatomic,assign) IBInspectable CGFloat lineWidth;
 
 //style
 - (instancetype) initWithFrame:(CGRect)frame style:(ZFProgressViewStyle)style;
